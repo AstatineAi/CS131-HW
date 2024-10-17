648c234b4ba72c060e0a0ece2ff4f4e3e563c029
Hengyu Ai <aihy2023@shanghaitech.edu.cn>
2024-10-17 23:53:49 +0800

    feat(test/studenttest.ml): fibonacci test

--- hw2/test/studenttests.ml
+++ hw2/test/studenttests.ml
@@ -108,6 +108,74 @@ let quick_power_rec (a : int) (b : int) =
   ]
 ;;
 
+(*RDI, RSI, RDX*)
+(*
+   fib:
+   ; Input:
+   ; RDI = n (index of Fibonacci number to calculate)
+   ; RSI = a (initial first value)
+   ; RDX = b (initial second value)
+
+   ; Base case: if n == 0, return a
+   cmp     rdi, 0          ; Compare n with 0
+   je      .return_a       ; If n == 0, return a (in RSI)
+
+   ; Base case: if n == 1, return b
+   cmp     rdi, 1          ; Compare n with 1
+   je      .return_b       ; If n == 1, return b (in RDX)
+
+   ; Recursive case: F(n) = F(n-1) + F(n-2)
+   ; Call nth_fibonacci(n-1, b, a + b)
+
+   sub     rdi, 1          ; n = n - 1 (decrement n)
+   mov     rax, rsi        ; rax = a
+   add     rax, rdx        ; rax = a + b (prepare new b = a + b)
+   mov     rsi, rdx        ; Move b into a's position (next recursive call)
+   mov     rdx, rax        ; Move a + b into b's position (next recursive call)
+
+   call    fib             ; Recursive call nth_fibonacci(n-1, b, a+b)
+   ret                     ; Return the result in rax
+
+   .return_a:
+   mov     rax, rsi        ; Return a (in RSI)
+   ret
+
+   .return_b:
+   mov     rax, rdx        ; Return b (in RDX)
+   ret
+*)
+let fib_rec (n : int) =
+  [ text
+      "fib"
+      [ Cmpq, [ ~$0; ~%Rdi ]
+      ; J Eq, [ ~$$"return_a" ]
+      ; Cmpq, [ ~$1; ~%Rdi ]
+      ; J Eq, [ ~$$"return_b" ]
+      ; Subq, [ ~$1; ~%Rdi ]
+      ; Movq, [ ~%Rsi; ~%Rax ]
+      ; Addq, [ ~%Rdx; ~%Rax ]
+      ; Movq, [ ~%Rdx; ~%Rsi ]
+      ; Movq, [ ~%Rax; ~%Rdx ]
+      ; Callq, [ ~$$"fib" ]
+      ; Retq, []
+      ]
+  ; text "return_a" [ Movq, [ ~%Rsi; ~%Rax ]; Retq, [] ]
+  ; text "return_b" [ Movq, [ ~%Rdx; ~%Rax ]; Retq, [] ]
+  ; text
+      "main"
+      [ Pushq, [ ~%Rbp ]
+      ; Movq, [ ~%Rsp; ~%Rbp ]
+      ; Movq, [ ~$n; ~%Rdi ]
+      ; Movq, [ ~$0; ~%Rsi ]
+      ; Movq, [ ~$1; ~%Rdx ]
+      ; Callq, [ ~$$"fib" ]
+      ; Movq, [ ~%Rbp; ~%Rsp ]
+      ; Popq, [ ~%Rbp ]
+      ; Retq, []
+      ]
+  ]
+;;
+
 let provided_tests : suite =
   [ Test ("My Tests", [ "assert", test_my ])
   ; Test
@@ -118,6 +186,9 @@ let provided_tests : suite =
         ; "quick power2", program_test (quick_power_rec 2 10) 1024L
         ; ( "quick power3"
           , program_test (quick_power_rec 3 36) 150094635296999121L )
+        ; "fib1", program_test (fib_rec 1) 1L
+        ; "fib2", program_test (fib_rec 10) 55L
+        ; "fib3", program_test (fib_rec 50) 12586269025L
         ] )
   ]
 ;;
