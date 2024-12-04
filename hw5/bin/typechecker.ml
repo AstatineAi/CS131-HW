3950f937eefd8176b8392d0d41ef5e21ccb955f6
Sizhe Zhao <prc.zhao@outlook.com>
2024-12-04 23:26:59 +0800

    fix: typecheck_block

--- hw5/bin/typechecker.ml
+++ hw5/bin/typechecker.ml
@@ -213,7 +213,7 @@ let typecheck_block (tc : Tctxt.t) (blk : Ast.block) (to_ret : ret_ty)
   | hd :: tl ->
     let tc', returns = typecheck_stmt tc hd to_ret in
     if returns then type_error hd "typ_block (typ_stmts)";
-    tc', returns
+    typecheck_block tc' tl to_ret
   | [] -> tc, true
 ;;
 
